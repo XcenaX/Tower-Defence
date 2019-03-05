6c08f9240984f769ec9046a31a02159d46ec64fd
XcenaX <40425017+XcenaX@users.noreply.github.com>
2019-03-05 12:43:04 +0600

    Tower Defence Prototype Alpha Version lmao

---
+++ Tower Defence Prototype/Assets/Scripts/MoveToWayPoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToWayPoints : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;       
+    public Transform[] wayPoints;
+    int curWayPoint = 0;
+
+    void Update()
+    {
+        if(curWayPoint < wayPoints.Length){
+        transform.LookAt(wayPoints[curWayPoint].position);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, wayPoints[curWayPoint].position, Time.deltaTime * speed);
+        if(Vector3.Distance(transform.position, wayPoints[curWayPoint].position) < 0.5f){
+           curWayPoint++;
+           if(curWayPoint == wayPoints.Length){
+               
+           }
+        }        
+        }
+    }
+}
---
+++ Tower Defence Prototype/Assets/Scripts/WaveSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WaveSpawn : MonoBehaviour
+{
+
+    [SerializeField]
+    private int waveSize;
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private float enemyInterval;  
+    [SerializeField] 
+    private Transform spawnPoint;
+    [SerializeField]
+    private int startTime;
+    [SerializeField]
+    private Vector3 offset;
+    private int enemyCount= 0;    
+    [SerializeField]
+    public Transform[] WayPoints;
+ 
+    private void Start()
+    {
+        InvokeRepeating("SpawnEnemy",startTime,enemyInterval);
+    }     
+
+    public void SpawnEnemy(){
+        enemyCount++;
+        GameObject enemy = GameObject.Instantiate(enemyPrefab,spawnPoint.position + offset, Quaternion.identity) as GameObject;
+        enemy.GetComponent<MoveToWayPoints>().wayPoints = WayPoints;        
+    }
+
+    private void Update(){
+        if(enemyCount == waveSize){
+            CancelInvoke("SpawnEnemy");
+        }
+    }
+}
